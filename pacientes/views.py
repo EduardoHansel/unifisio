fab3448c0c124d46496e198ca928b95b56809264
EduardoHansel <166572116+EduardoHansel@users.noreply.github.com>
2025-06-05 17:24:04 -0300

    Update views.py

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,6 +1,10 @@
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+
+from .forms import PacienteForm
 from .models import Paciente
 
 # Create your views here.
@@ -9,28 +13,30 @@ def say_hello(request):
     return HttpResponse("Hello World")
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def criar_paciente(request):
-    if request.method == 'POST':
-        nome_do_paciente = request.POST.get("nome")
-        cpf_do_paciente = request.POST.get("cpf")
-        sexo_do_paciente = request.POST.get("sexo")
-        data_de_mascimento_paciente = request.POST.get("data_nascimento")
-        contato_1_paciente = request.POST.get("contato_1")
-        contato_2_paciente = request.POST.get("contato_2")
+    try:
+        data = json.loads(request.body)
+        form = PacienteForm(data)
 
-        if nome_do_paciente and cpf_do_paciente and sexo_do_paciente and data_de_mascimento_paciente and contato_1_paciente and contato_2_paciente:
-            novo_paciente = Paciente(nome=nome_do_paciente, cpf=cpf_do_paciente, sexo=sexo_do_paciente, data_nascimento=data_de_mascimento_paciente, contato_1=contato_1_paciente, contato_2=contato_2_paciente)
-            print(f"Novo paciente criado: {novo_paciente.nome}")
-            print(f"Total de pacientes na lista: {len(Paciente.lista_pacientes)}")
+        if form.is_valid():
+            novo_paciente = form.save()
+
+            print(f"Novo paciente salvo no banco de dados: {novo_paciente.nome}, ID: {novo_paciente.id}")
 
             dados_resposta = {
-                'id': Paciente.current_id,
+                'id': novo_paciente.id,
                 'nome': novo_paciente.nome,
                 'cpf': novo_paciente.cpf,
-                'message': 'Paciente criado com sucesso!'
+                'message': 'Paciente criado com sucesso'
             }
-
             return JsonResponse(dados_resposta, status=201)
         else:
-            return JsonResponse({'error': 'Nome e CPF são campos obrigatórios.'}, status=400)
-    return JsonResponse({'error': 'Método GET não é permitido para esta URL'}, status=405)
+            # Se o formulário for inválido, retorna os erros de validação
+            return JsonResponse({'errors': form.errors}, status=400)
+
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Corpo da requisição JSON inválido.'}, status=400)
+    except Exception as e:
+        # Captura outros erros inesperados
+        return JsonResponse({'error': f'Ocorreu um erro interno: {str(e)}'}, status=500)
